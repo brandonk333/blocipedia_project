e3fad3fb6add192b04feb5bbfec812779b0f109a
btkunisch <btkunisch_@hotmail.com>
2016-03-25 18:51:51 +0000

    wiki_collaborators completed 3-25

--- app/policies/wiki_policy.rb
+++ app/policies/wiki_policy.rb
@@ -5,10 +5,50 @@ class WikiPolicy < ApplicationPolicy
   end
   
   def show?
-    (user.present? && ((user.role == 'premium_user') || (user.role == 'admin'))) || record.private != true
+    record.private != true || (user.present? && ((user.role == 'admin') || record.user == user || record.users.include?(user)))  
+  end
+
+  def update?
+    show? 
+  end
+
+  def edit?
+    show? 
   end
   
-  def destroy?
-    user.present? && (record.user == user || user.admin?)
+  #def destroy?
+    #user.present? && (record.user == user || user.admin?)
+  #end
+  
+  class Scope
+    attr_reader :user, :scope
+
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      wikis = []
+      if user.present? && user.role == 'admin'
+        wikis = scope.all 
+      elsif user.present? && user.role == 'premium_user'
+        all_wikis = scope.all
+        all_wikis.each do |wiki|
+          if wiki.private != true || wiki.user == user || wiki.users.include?(user)
+            wikis << wiki 
+          end
+      end
+      else 
+        all_wikis = scope.all
+        wikis = []
+        all_wikis.each do |wiki|
+          if wiki.private != true || wiki.users.include?(user)
+            wikis << wiki 
+          end
+        end
+      end
+      wikis  
+    end
   end
 end
