a151297985703fa47ecbfbfc348bca32d66998fd
btkunisch <btkunisch_@hotmail.com>
2016-03-26 02:04:51 +0000

    updated_views completed 3-25

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,16 @@ class ApplicationController < ActionController::Base
   
   before_action :authenticate_user!
   
-   rescue_from Pundit::NotAuthorizedError do |exception|
-     redirect_to root_url, alert: exception.message
-   end  
+  rescue_from Pundit::NotAuthorizedError do |exception|
+    redirect_to root_url, alert: exception.message
+  end
+   
+  protected
+
+  def after_sign_in_path_for(resource)
+     wikis_path
+  end
 end
+   
 
 
